689c9b14f8dfb238653639878b7521760ac88b10
QHwan <poter1026@gmail.com>
2019-04-02 20:25:12 +0900

    basic crawler

--- web_crawling.py
+++ web_crawling.py
@@ -1,5 +1,10 @@
-import requests	# request is most basic paser
+import requests	# request is most basic parser, only returns string data
+from bs4 import BeautifulSoup # BS parses html -> meaningful python class structure
+import json
+import os
 
+# location of python file: useful expression!
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 page_address = "https://beomi.github.io/beomi.github.io_old"
 
 ## HTTP GET Request
@@ -7,16 +12,29 @@ req = requests.get(page_address)
 
 ## HTTP source
 source = req.text
-print(source)
+
+## It converts text -> python class
+soup = BeautifulSoup(source, 'html.parser')
+
+## Find titles from CSS Selector: CSS grammar?
+my_titles = soup.select( 'h3 > a') # my_titles is the list of instances
+
+data = {}
+
+for title in my_titles:
+	# property of Tag(ex: href property)
+	data[title.text] = title.get('href')
+
+with open(os.path.join(BASE_DIR, 'result.json'), 'w+') as json_file:
+	json.dump(data, json_file)
+
+
 
 ## HTTP Header
 header = req.headers
-print(header)
 
 ## HTTP Status: 200 == OK
 status = req.status_code
-print(status)
 
 ## Check the normal of HTTP
 is_ok = req.ok
-print(is_ok)
